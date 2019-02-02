53403ce67b9e8ec92338510064d14aeee99d8397
mirrorbitmirror <32906059+mirrorbitmirror@users.noreply.github.com>
2019-02-02 22:47:17 +0300

    Initial commit

---
+++ add_user.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Odmen
+ * Date: 2/1/2019
+ * Time: 6:07 PM
+ */
+
+//Выводим сообщение об удачной регистрации
+if(isset($_GET['status']) and $_GET['status'] == 'ok')
+    echo '<b>Вы успешно зарегистрировались! Пожалуйста активируйте свой аккаунт!</b>';
+
+function emailValid($email)
+{
+    if(function_exists('filter_var')){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        if(!preg_match("/^[a-z0-9_.-]+@([a-z0-9]+\.)+[a-z]{2,6}$/i", $email)){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+//Функция вывода ошибок
+function showErrorMessage($data)
+{
+    $err = '<ul>'."\n";
+
+    if(is_array($data)) {
+        foreach($data as $val)
+            $err .= '<li style="color:red;">'. $val .'</li>'."\n";
+    }
+    else
+        $err .= '<li style="color:red;">'. $data .'</li>'."\n";
+        $err .= '</ul>'."\n";
+        return $err;
+
+}
+
+$err = array();
+if(isset($_POST['submit'])) {
+    //Утюжим пришедшие данные
+    if(empty($_POST['login']))
+        $err[] = 'Поле Логин не может быть пустым!';
+    if(empty($_POST['name']))
+        $err[] = 'Поле Имя  не может быть пустым';
+    if(empty($_POST['surname']))
+        $err[] = 'Поле Фамилия пароля не может быть пустым';
+    if(empty($_POST['is_admin']))
+        $err[] = 'Поле Тип пользователя не может быть пустым';
+    if(empty($_POST['email']))
+        $err[] = 'Поле E-mail не может быть пустым';
+    else {
+        if(emailValid($_POST['email']) === false)
+            $err[] = 'Не правильно введен E-mail'."\n";
+    }
+    if(count($err) > 0){
+        echo showErrorMessage($err);
+    } else {
+        /*Проверяем существует ли у нас такой пользователь в БД*/
+        $sql = 'SELECT `login` FROM `users`	WHERE `login` = :login';
+        //Подготавливаем PDO выражение для SQL запроса
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if(count($rows) > 0) {
+            $err[] = 'К сожалению Логин: <b>' . $_POST['login'] . '</b> занят!';
+            echo showErrorMessage($err);
+        } else {
+                /*Если все хорошо, пишем данные в базу*/
+                $sql = 'INSERT INTO `users`	VALUES(:login, :first_name,	:last_name,	:email,	:is_admin)';
+                $stmt = $db->prepare($sql);
+                $stmt->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
+                $stmt->bindValue(':first_name', $_POST['name'], PDO::PARAM_STR);
+                $stmt->bindValue(':last_name', $_POST['surname'], PDO::PARAM_STR);
+                $stmt->bindValue(':email', $_POST['email'], PDO::PARAM_STR);
+                $stmt->bindValue(':is_admin', $_POST['is_admin'], PDO::PARAM_STR);
+                $stmt->execute();
+                echo '<span style="color:red;">Добавил пользователя успешно</span>';
+                exit;
+            }
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Odmen
+ * Date: 2/1/2019
+ * Time: 6:07 PM
+ */
+//Устанавливаем кодировку и вывод всех ошибок
+header('Content-Type: text/html; charset=UTF8');
+error_reporting(E_ALL);
+
+//Адрес базы данных
+define('DB_SERVER','localhost');
+
+//Логин БД
+define('DB_USER','root');
+
+//Пароль БД
+define('DB_PASSWORD','');
+
+//Имя Базы Данных
+define('DATABASE','two_up');
+
+//Адрес хоста сайта
+define('HOST','http://'. $_SERVER['HTTP_HOST'] .'/');
+
+//Подключение к базе данных mySQL с помощью PDO
+try {
+    $db = new PDO('mysql:host=localhost;dbname='.DATABASE, DB_USER, DB_PASSWORD, array(
+        PDO::ATTR_PERSISTENT => true
+    ));
+
+} catch (PDOException $e) {
+    print "Ошибка соединеия!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
+    <h3>Найти пользователя:</h3>
+<form name="search" method="get">
+    <table>
+        <tr>
+            <td>Логин:</td>
+            <td><input type="text" size="30" name="login"></td>
+        </tr>
+        <tr>
+            <td>Имя:</td>
+            <td><input type="text" size="30" name="name"></td>
+        </tr>
+        <tr>
+            <td>Фамилия:</td>
+            <td><input type="text" size="30" name="surname"></td>
+        </tr>
+        <tr>
+            <td>E-mail:</td>
+            <td><input type="text" size="30" name="email"></td>
+        </tr>
+        <tr>
+            <td>Тип пользователя:</td>
+            <td><input type="radio" name="is_admin" value="2">Admin<input type="radio" name="is_admin" value="1">User</td>
+        </tr>
+        <tr>
+            <td> </td>
+            <td colspan="2"><input type="submit" value="find" name="submit"></td>
+        </tr>
+    </table>
+
+</form>
+
+<hr>
+<h3>Добавить пользователя:</h3>
+    <form action="" method="POST">
+        <table>
+            <tr>
+                <td>Логин:</td>
+                <td><input type="text" size="30" name="login"></td>
+            </tr>
+            <tr>
+                <td>Имя:</td>
+                <td><input type="text" size="30" name="name"></td>
+            </tr>
+            <tr>
+                <td>Фамилия:</td>
+                <td><input type="text" size="30" name="surname"></td>
+            </tr>
+            <tr>
+                <td>E-mail:</td>
+                <td><input type="text" size="30" name="email"></td>
+            </tr>
+            <tr>
+                <td>Тип пользователя:</td>
+                <td><input type="radio" name="is_admin" value="2">Admin<input type="radio" name="is_admin" value="1" checked>User</td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td colspan="2"><input type="submit" value="Зарегистроваться" name="submit"></td>
+            </tr>
+        </table>
+    </form>
+
+<?php
+//Подключаем добавление пользователей в БД
+include 'add_user.php';
+//Поиск
+if(empty($_GET)) {
+    return false;
+} else {
+$sql = "SELECT * FROM `users` WHERE `login` LIKE :login AND `first_name` LIKE :name AND `last_name` LIKE :surname AND `email` LIKE :email AND `is_admin` LIKE :is_admin";
+
+//Подготавливаем PDO выражение для SQL запроса
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':login', "%". $_GET['login'] . "%", PDO::PARAM_STR);
+$stmt->bindValue(':name', "%". $_GET['name'] . "%", PDO::PARAM_STR);
+$stmt->bindValue(':surname', "%". $_GET['surname'] . "%", PDO::PARAM_STR);
+$stmt->bindValue(':email', "%". $_GET['email'] . "%", PDO::PARAM_STR);
+$stmt->bindValue(':is_admin', "%". $_GET['is_admin'] . "%", PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//Таблица
+echo '<table cellpadding="5" cellspacing="0" border="1">';
+echo '<tr><td>Логин</td><td>Имя</td><td>Фамилия</td><td>E-mail</td><td>Тип пользователя</td></tr>';
+foreach ($rows as $key => $value) {
+    echo "<tr>";
+    foreach ($value as $data)
+        echo "<td>".$data."</td>";
+    echo "</tr>";
+}
+echo "</table>";
+}
